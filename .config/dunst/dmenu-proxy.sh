591ed83415bbf864e20905098a368bebb7a95ef0
Jackson Delahunt <jackson@stemn.com>
2026-02-02 09:03:49 +1100

    fix-dmenu-proxy

--- .config/dunst/dmenu-proxy.sh
+++ .config/dunst/dmenu-proxy.sh
@@ -1,8 +1,19 @@
-# Pass arguments to dmenu and capture output
-DMENU_RESULT=$(dmenu $@)
+#!/bin/bash
 
-# If the action selected was "view notification" switch to the app
-if [ "$DMENU_RESULT" = "${DMENU_RESULT##View}" ]; then
+# Pass arguments to dmenu and capture output
+args=()
+for a in "$@"; do
+  if [[ "$a" == rgb:*/*/*/* ]]; then
+    args+=("${a%/*}")
+  else
+    args+=("$a")
+  fi
+done
+
+DMENU_RESULT="$(dmenu "${args[@]}")"
+
+# If the action selected was "activate application" switch to the app
+if [ "$DMENU_RESULT" = "${DMENU_RESULT##Activate}" ]; then
   APP_NAME=$(echo $DMENU_RESULT | sed -E 's/[^(]*\(([^)]*).*/\1/')
   ~/.config/i3/move-to-next-window-of-type.sh $APP_NAME
 fi
